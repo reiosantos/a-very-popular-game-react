5c24da9f4ceb2f1e8fa0b2461e33354d68473929
santos <ronireiosantos@gmail.com>
2020-07-15 19:10:44 +0300

    - Add shuffle algo

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ class Home extends React.Component<any> {
 	cols: number[] = []
 	colorsArray: string[] = [];
 	maxNumColors = 0;
+	state: any = {}
 
 	constructor(props: any) {
 		super(props);
@@ -33,63 +34,79 @@ class Home extends React.Component<any> {
 
 	getArray = (num: number) => Array(num).fill(1).map((i, idx) => idx)
 
-	private shuffleMe = (row: number, col: number) => (event: any) => {
-		const origin = [row - 1, col - 1].filter(v => v > -1);
-		const bottomRight = [row + 1, col + 1].filter(v => v < this.gridCols);
-		const bottomLeft = [row + 1, col - 1].filter((v, i) => {
-			if (i === 0) {
-				return v < this.gridRows;
-			}
-			return v > -1;
-		});
-		const topRight = [row - 1, col + 1].filter((v, i) => {
-			if (i === 0) {
-				return v > -1;
-			}
-			return v < this.gridCols;
-		});
+	private markMe = (b_row: number, b_col: number) => (event: any) => {
+		const origin = [b_row - 1, b_col - 1].filter(v => v > -1);
+		if (origin.length !== 2) return;
+
+		const tileHasOrigin = origin.length === 2 && this.colors[origin.join("")] === this.colors[`${b_row}${b_col}`];
+		if (!tileHasOrigin) {
+			alert('----Has no origin---');
+			return;
+		}
+
+		const [row, col] = origin;
 
 		const top = [row - 1, col].filter(v => v > -1);
 		const right = [row, col + 1].filter(v => v < this.gridCols);
 		const bottom = [row + 1, col].filter(v => v < this.gridRows);
 		const left = [row, col - 1].filter(v => v > -1);
 
-		// TODO: add algorithm
+		this.setState({origin, top, left, bottom, right, selected: `${b_row}${b_col}` });
+	}
 
+	private shuffleMe = (colorIdx: number) => (event: any) => {
+		const {top, left, bottom, right} = this.state;
 
+		this.colors[top.join("")] =
+			this.colors[left.join("")] =
+				this.colors[bottom.join("")] =
+					this.colors[right.join("")] = colorIdx;
 
 		this.setState({});
-	}
+	};
 
 	render() {
 		return (
-			<table className="table w-auto m-auto">
-				<tbody>
-				{this.rows.map((i, idx) => {
-					this.grid[idx] = [];
-					return <tr key={i * idx}>
-						{
-							this.cols.map((j, idx1) => {
-								this.grid[idx][idx1] = j;
-								if (this.colors[`${i}${j}`] === undefined){
-									this.colors[`${i}${j}`] = Math.floor(Math.random() * this.maxNumColors)
-								}
+			<React.Fragment>
+				<table className="table w-auto m-auto">
+					<tbody>
+					{this.rows.map((i, idx) => {
+						this.grid[idx] = [];
+						return <tr key={i * idx}>
+							{
+								this.cols.map((j, idx1) => {
+									this.grid[idx][idx1] = j;
+									if (this.colors[`${i}${j}`] === undefined){
+										this.colors[`${i}${j}`] = Math.floor(Math.random() * this.maxNumColors)
+									}
 
-								const background = this.colorsArray[this.colors[`${i}${j}`]];
+									const background = this.colorsArray[this.colors[`${i}${j}`]];
 
-								return (
-									<td
-										onClick={this.shuffleMe(i, j)}
-										style={{backgroundColor: background}}
-										key={j * idx1}>{i} {j}
-									</td>
-								);
-							})
-						}
-					</tr>;
+									return (
+										<td
+											onClick={this.markMe(i, j)}
+											className={this.state.selected === `${i}${j}` ? 'selected' : ''}
+											style={{backgroundColor: background}}
+											key={j * idx1}>{i} {j}
+										</td>
+									);
+								})
+							}
+						</tr>;
+					})}
+					</tbody>
+				</table>
+
+				{ this.colorsArray.map((c, i) => {
+					return <button
+						key={i}
+						className="w-auto m-auto btn btn-small btn"
+						style={{backgroundColor: c}}
+						onClick={this.shuffleMe(i)}>
+						{c}
+					</button>;
 				})}
-				</tbody>
-			</table>
+			</React.Fragment>
 		);
 	}
 }
